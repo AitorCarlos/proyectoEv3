722bf24193f79fae395f98a27ab6a42dbda35089
Unknown <alexsandro.ruiz@ikasle.egibide.org>
2018-05-15 09:30:12 +0200

    Coneccion y peticines

--- RAIZ/DATOS/PR/Master_ESport/src/Views/Peticiones/Peticiones.java
+++ RAIZ/DATOS/PR/Master_ESport/src/Views/Peticiones/Peticiones.java
@@ -18,6 +18,8 @@ public class Peticiones extends javax.swing.JPanel {
     public Peticiones() {
         initComponents();
         listaPeticiones();
+        
+        
     }
     
     public void listaPeticiones(){
@@ -49,9 +51,11 @@ public class Peticiones extends javax.swing.JPanel {
             showMessageDialog(this, "Peticion aceptada");
             
         }
+        catch (ArrayIndexOutOfBoundsException e){
+        
+            showMessageDialog(this, "Seleccione una peticion por favor");
+        }
         catch (Exception e) {
-            
-            showMessageDialog(this, e.getMessage());
         }
     }
     
@@ -67,9 +71,11 @@ public class Peticiones extends javax.swing.JPanel {
             showMessageDialog(this, "Peticion denegada");
             
         }
+        catch (ArrayIndexOutOfBoundsException e){
+        
+            showMessageDialog(this, "Seleccione una peticion por favor");
+        }
         catch (Exception e) {
-            
-            showMessageDialog(this, e.getMessage());
         }
 
     }
@@ -88,9 +94,11 @@ public class Peticiones extends javax.swing.JPanel {
             showMessageDialog(this, "Peticion aceptada");
             
         }
+        catch (ArrayIndexOutOfBoundsException e){
+        
+            showMessageDialog(this, "Seleccione una peticion por favor");
+        }
         catch (Exception e) {
-            
-            showMessageDialog(this, e.getMessage());
         }
     }
     
@@ -106,9 +114,11 @@ public class Peticiones extends javax.swing.JPanel {
             showMessageDialog(this, "Peticion denegada");
             
         }
+        catch (ArrayIndexOutOfBoundsException e){
+        
+            showMessageDialog(this, "Seleccione una peticion por favor");
+        }
         catch (Exception e) {
-            
-            showMessageDialog(this, e.getMessage());
         }
 
     }
@@ -127,9 +137,11 @@ public class Peticiones extends javax.swing.JPanel {
             showMessageDialog(this, "Peticion aceptada");
             
         }
+        catch (ArrayIndexOutOfBoundsException e){
+        
+            showMessageDialog(this, "Seleccione una peticion por favor");
+        }
         catch (Exception e) {
-            
-            showMessageDialog(this, e.getMessage());
         }
     }
     
@@ -138,16 +150,18 @@ public class Peticiones extends javax.swing.JPanel {
         try {
             
             ArrayList <Jugador> d = Master_ESport.verPeticionJugador();
-            Master_ESport.borrarPeticion(d.get(r).getNickname());
+            Master_ESport.borrarPeticion(d.get(r).getNombre());
             
             Master_ESport.list.remove(r);
             
             showMessageDialog(this, "Peticion denegada");
             
         }
+        catch (ArrayIndexOutOfBoundsException e){
+        
+            showMessageDialog(this, "Seleccione una peticion por favor");
+        }
         catch (Exception e) {
-            
-            showMessageDialog(this, e.getMessage());
         }
 
     }
@@ -192,60 +206,42 @@ public class Peticiones extends javax.swing.JPanel {
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(47, 47, 47)
+                .addComponent(jButton2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 113, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addGap(162, 162, 162))
             .addGroup(layout.createSequentialGroup()
-
+                .addGap(25, 25, 25)
+                .addComponent(jDuenno, javax.swing.GroupLayout.PREFERRED_SIZE, 351, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(34, 34, 34)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton1)
+                    .addComponent(jButton2))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 49, Short.MAX_VALUE)
+                .addComponent(jDuenno, javax.swing.GroupLayout.PREFERRED_SIZE, 156, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(38, 38, 38))
         );
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-       
-        if(jpl.getSelectedIndex()==-1){
 
-            showMessageDialog(this, "Seleccione una peticion por favor");
-        }
-        else{
-            if(jpl.getSelectedValue().substring(15, 21).equals("duenno")){
-
-                aceptarPeticionDuenno(jpl.getSelectedIndex());
-            }
-            else if(jpl.getSelectedValue().substring(15, 21).equals("equipo")){
-
-                aceptarPeticionEquipo(jpl.getSelectedIndex());
-            }
-            else if(jpl.getSelectedValue().substring(15, 22).equals("jugador")){
-
-                aceptarPeticionJugador(jpl.getSelectedIndex());
-            }
-        }
- 
+        aceptarPeticionDuenno(jpl.getSelectedIndex());
+        aceptarPeticionEquipo(jpl.getSelectedIndex());
+        aceptarPeticionJugador(jpl.getSelectedIndex());
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-       
-        if(jpl.getSelectedIndex()==-1){
 
-            showMessageDialog(this, "Seleccione una peticion por favor");
-        }
-        else{
-            
-            if(jpl.getSelectedValue().substring(15, 21).equals("duenno")){
-
-                denegarPeticionDuenno(jpl.getSelectedIndex());
-            }
-            else if(jpl.getSelectedValue().substring(15, 21).equals("equipo")){
-
-                denegarPeticionEquipo(jpl.getSelectedIndex());
-            }
-            else if(jpl.getSelectedValue().substring(15, 22).equals("jugador")){
-
-                denegarPeticionJugador(jpl.getSelectedIndex());
-            }
-        }
+        denegarPeticionDuenno(jpl.getSelectedIndex());
+        denegarPeticionEquipo(jpl.getSelectedIndex());
+        denegarPeticionJugador(jpl.getSelectedIndex());
     }//GEN-LAST:event_jButton2ActionPerformed
 
 
